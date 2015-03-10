c5ba556e8402612facc63109aa06283be07abfad
Ben Callis <bencallis@Bens-MacBook-Pro.local>
2015-03-10 13:52:11 -0600

    First Commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'port': process.env.PORT || 8080,
+	'database': 'mongodb://bencallis1:Katief9727@proximus.modulusmongo.net:27017/E4xinagu',
+	'secret': 'iloveangularjs'
+};
---
+++ public/app/app.routes.js
@@ -0,0 +1,50 @@
+angular.module('app.routes', ['ngRoute'])
+
+.config(function($routeProvider, $locationProvider) {
+
+	$routeProvider
+
+		// route for the home page
+		.when('/', {
+			templateUrl : 'app/views/pages/home.html'
+		})
+		
+		// login page
+		.when('/login', {
+			templateUrl : 'app/views/pages/login.html',
+   			controller  : 'mainController',
+    			controllerAs: 'login'
+		})
+		
+		// show all users
+		.when('/users', {
+			templateUrl: 'app/views/pages/users/users.html',
+			controller: 'userController',
+			controllerAs: 'user'
+		})
+		//create a new user login 
+		.when('/create', {
+			templateUrl: 'app/views/pages/create.html',
+			controller: 'userController',
+			controllerAs: 'user'
+		})
+
+		// form to create a new user
+		// same view as edit page
+		.when('/users/create', {
+			templateUrl: 'app/views/pages/users/single.html',
+			controller: 'userCreateController',
+			controllerAs: 'user'
+		})
+
+		// page to edit a user
+		.when('/users/:user_id', {
+			templateUrl: 'app/views/pages/users/single.html',
+			controller: 'userEditController',
+			controllerAs: 'user'
+		});
+
+
+	$locationProvider.html5Mode(true);
+
+});
