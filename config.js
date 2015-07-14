2d7ad8bad0f3d1d47247f9b25a01ceddda371c8d
bencallis1 <bencallis1@gmail.com>
2015-07-14 13:03:24 -0600

    added new auth platform

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	'port': process.env.PORT || 8080,
-	'database': 'mongodb://bencallis1:Katief9727@ds041248.mongolab.com:41248/gooddeeds',
-	'secret': 'iloveangularjs'
-};
--- public/app/app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-angular.module('userApp', ['ngResource','ngAnimate', 'routerApp','door3.css','socialCtrl', 'userCtrl', 'userService','dribbleService']);
-
-
-
-
-
