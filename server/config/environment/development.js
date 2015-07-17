7d1833387219bbec81ab79f2fa7527b1662def6a
bencallis1 <bencallis1@gmail.com>
2015-07-16 18:36:23 -0600

    Made a ton of changes

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/meanstackcrm-dev'
+  },
+
+  seedDB: true
+};
